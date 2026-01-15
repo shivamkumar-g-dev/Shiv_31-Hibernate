01eab222bf7febdeb058ad27b6dc7f7bf6c256bb
shivamkumar-g-dev <83403644+shivamkumar-g-dev@users.noreply.github.com>
2026-01-16 01:27:35 +0530

    Initial commit

---
+++ src/main/java/shiv_31/Employee.java
@@ -0,0 +1,93 @@
+package shiv_31;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name="empp")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "e_Name")
+	
+	private String name;
+	private String gender;
+	private int salary;
+	
+	
+	
+	public Employee() {
+		super();
+	}
+
+
+
+	public Employee( String name, String gender, int salary) {
+		super();
+		this.name = name;
+		this.gender = gender;
+		this.salary = salary;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+
+	public int getSalary() {
+		return salary;
+	}
+
+
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + "]";
+	}
+	
+
+}
+	
---
+++ src/main/java/shiv_31/main/EmployeeRunner.java
@@ -0,0 +1,49 @@
+package shiv_31.main;
+
+
+//import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+//import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+//import org.hibernate.boot.Metadata;
+//import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import shiv_31.Employee;
+
+public class EmployeeRunner {
+
+	public static void main(String[] args)  {
+		
+		Employee emp = new Employee("Preet", "Female",38983);
+		
+//		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+//		SessionFactory sessionFactory = cfg.buildSessionFactory();
+//		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		
+//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+//		Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+//		SessionFactory sessionFactory = metadata.buildSessionFactory();
+		
+		
+//		Metadata metadata = new MetadataSources(new StandardServiceRegistryBuilder() ).getMetadataBuilder().build();
+		
+		
+		SessionFactory sessionFactory = new MetadataSources(new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build()).getMetadataBuilder().build().buildSessionFactory();
+		
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+	
+		session.persist(emp);
+		tx.commit();
+		
+		
+//		Employee employee = session.find(Employee.class,19);
+//		System.out.println(employee);
+		
+		
+	}
+
+}
