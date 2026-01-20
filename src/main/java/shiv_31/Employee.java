4d3edddf5175807f672350c9e869fb31f9a7453d
shivamkumar-g-dev <83403644+shivamkumar-g-dev@users.noreply.github.com>
2026-01-20 11:38:01 +0530

    update

--- src/main/java/shiv_31/Employee.java
+++ src/main/java/shiv_31/Employee.java
@@ -1,29 +1,39 @@
 package shiv_31;
 
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+<<<<<<< Updated upstream
 
 @Entity(name="empp")
+=======
+import jakarta.persistence.OneToOne;
+
+@Entity(name = "empp")
+>>>>>>> Stashed changes
 public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	@Column(name = "e_Name")
-	
 	private String name;
 	private String gender;
 	private int salary;
+<<<<<<< Updated upstream
 	
 	
+=======
+>>>>>>> Stashed changes
 	
+	@OneToOne
+	private Address address;
+
 	public Employee() {
 		super();
 	}
 
+<<<<<<< Updated upstream
 
 
 	public Employee( String name, String gender, int salary) {
@@ -31,63 +41,62 @@ public class Employee {
 		this.name = name;
 		this.gender = gender;
 		this.salary = salary;
+=======
+	public Employee(String name, String gender, int salary) {
+		this.name = name;
+		this.gender = gender;
+		this.salary = salary;
+	}
+	
+	
+
+	public Address getAddress() {
+		return address;
+>>>>>>> Stashed changes
 	}
 
-
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 
 	public int getId() {
 		return id;
 	}
 
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-
 
 	public String getName() {
 		return name;
 	}
 
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
 
 	public String getGender() {
 		return gender;
 	}
 
-
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
-
 
 	public int getSalary() {
 		return salary;
 	}
 
-
-
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
 
-
-
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + "]";
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + ", address="
+				+ address + "]";
 	}
+
 	
 
-}
-	
+}	
--- src/main/java/shiv_31/main/EmployeeRunner.java
+++ src/main/java/shiv_31/main/EmployeeRunner.java
@@ -1,6 +1,11 @@
 package shiv_31.main;
 
 
+<<<<<<< Updated upstream
+=======
+//import java.util.List;
+
+>>>>>>> Stashed changes
 //import org.hibernate.SessionFactory;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -9,13 +14,110 @@ import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 //import org.hibernate.boot.Metadata;
 //import org.hibernate.boot.registry.StandardServiceRegistry;
+<<<<<<< Updated upstream
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+=======
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//import org.hibernate.query.MutationQuery;
+//import org.hibernate.query.Query;
+
+import com.shiv.config.EmpConfiguration;
+>>>>>>> Stashed changes
 
 import shiv_31.Employee;
 
 public class EmployeeRunner {
 
+<<<<<<< Updated upstream
 	public static void main(String[] args)  {
+=======
+
+
+//	public static void delById(int id, Session session) {
+//	HQL delete query
+//		MutationQuery query = session.createMutationQuery("delete from empp where id = :id");
+//		query.setParameter("id", id);
+//		query.executeUpdate();
+//	}
+	
+	
+	public static void main(String[] args) {
+
+		Employee emp1 = new Employee("Avya", "FeMale", 64548);
+//		Employee emp2 = new Employee("Nitin", "FeMale", 64548, "IND");
+//		Employee emp3 = new Employee("Neelam", "FeMale", 64548, "IND");
+//		Employee emp4 = new Employee("Jack", "Male", 64548, "IND");
+//		Employee emp5 = new Employee("Atharv", "Male", 64548, "IND");
+//		Employee emp6 = new Employee("Naitik", "Male", 64548, "IND");
+//		Employee emp7 = new Employee("Preetam", "Male",38983,"IND");
+		
+		
+		
+		Session session = EmpConfiguration.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		
+		
+		
+//		MutationQuery namedQuery = session.createNamedMutationQuery("updateAnEmployee");
+//		namedQuery.setParameter("n", "Shreya");
+//		namedQuery.setParameter("i", 10);
+//		namedQuery.executeUpdate();q
+		
+//		
+//		MutationQuery namedQuery = session.createNamedMutationQuery("deleteEmployeeById");
+//		namedQuery.setParameter("i", 10);
+//		namedQuery.executeUpdate();
+//		
+		
+		
+		
+		
+		
+		
+//		session.persist(emp1);
+//		session.persist(emp2);
+//		session.persist(emp3);
+//		session.persist(emp4);
+//		session.persist(emp5);
+//		session.persist(emp6);
+//		session.persist(emp7);
+		
+		
+//		Query <Employee>query=session.createQuery("from empp",Employee.class);
+//		List list=query.list(); 
+//		System.out.println(list);
+		
+
+//		Example of HQL to get all the records
+//		Query query1=session.createQuery("from empp",Employee.class);
+//		List list=query1.list();  
+//		System.out.println(list);
+//		
+//		HQL to get records with pagination
+//		query1.setFirstResult(2);
+//		query1.setMaxResults(5);
+//		System.out.println(query1.list());
+
+//		Query q=session.createQuery("select max(name) from empp");  
+//		List list1=q.list();  
+//		System.out.println(list1);  
+
+//		HQL update query Using Named Parameters
+//		MutationQuery mutationQuery = session.createMutationQuery("update empp set name=:n where id=:i");  
+//		mutationQuery.setParameter("n","Pankaj");  
+//		mutationQuery.setParameter("i",4);  
+//		System.out.println("status: "+mutationQuery.executeUpdate());  
+
+//		HQL delete query
+//		MutationQuery query=session.createMutationQuery("delete from empp where id = 17");  
+//		query.executeUpdate();  
+//		
+//		Query query11=session.createQuery("from empp",Employee.class);
+//		List list1=query11.list();  
+//		System.out.println(list1);
+
+		
+>>>>>>> Stashed changes
 		
 		Employee emp = new Employee("riya", "Female",38983);
 		
@@ -36,9 +138,22 @@ public class EmployeeRunner {
 		Session session = sessionFactory.openSession();
 		Transaction tx = session.beginTransaction();
 	
+<<<<<<< Updated upstream
 		session.persist(emp);
 		tx.commit();
 		
+=======
+		session.persist(emp1);
+		tx.commit();
+		
+//		Query query1 = session.createQuery("from empp", Employee.class);
+//		List list = query1.list();
+//		System.out.println(list);
+		
+		
+//		session.close();
+		
+>>>>>>> Stashed changes
 		
 //		Employee employee = session.find(Employee.class,19);
 //		System.out.println(employee);
